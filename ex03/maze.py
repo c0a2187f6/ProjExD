0a186606017496ddc59a931edd4efbbcb043b2a4
C0A21087 <c0a21087f6@edu.teu.ac.jp>
2022-12-06 14:45:01 +0900

    練習6,7

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,7 +6,20 @@ def key_down(event):
 
 def key_up(event):
   global key
-  key = event.keysym
+  key = " "
+
+def main_proc():
+  global cx, cy
+  if key == "Up":
+    cy -= 20
+  if key == "Down":
+    cy += 20
+  if key == "Left":
+    cx -= 20
+  if key =="Right":
+    cx += 20
+  canvas.coords("kokaton", cx, cy)
+  root.after(10, main_proc)
 
 if __name__ == "__main__":
   root = tk.Tk()
@@ -19,5 +32,6 @@ if __name__ == "__main__":
   canvas.create_image(cx, cy, image=kokaton, tag="kokaton")
   key = " "
   root.bind("<KeyPress>", key_down)
-  root.bind("<KeyRelease>", key_down)
+  root.bind("<KeyRelease>", key_up)
+  main_proc()
   root.mainloop() 
