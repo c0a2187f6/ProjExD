3a080b293055aae8af4b4a84e1dba811c971a4ed
C0A21087 <c0a21087f6@edu.teu.ac.jp>
2022-12-06 14:34:16 +0900

    練習5まで

---
+++ ex03/maze.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+def key_down(event):
+  global key 
+  key = event.keysym
+
+def key_up(event):
+  global key
+  key = event.keysym
+
+if __name__ == "__main__":
+  root = tk.Tk()
+  root.title("迷えるこうかとん")
+  canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+  canvas.pack()
+
+  kokaton = tk.PhotoImage(file="fig/4.png")
+  cx, cy = 300, 400
+  canvas.create_image(cx, cy, image=kokaton, tag="kokaton")
+  key = " "
+  root.bind("<KeyPress>", key_down)
+  root.bind("<KeyRelease>", key_down)
+  root.mainloop() 
